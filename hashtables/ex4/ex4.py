14ca7cc4ebf9ccc0d9cae7608bb29d157ecde401
mikejeter <mjeter45@yahoo.com>
2020-08-16 21:24:32 -0400

    Implemented has_negatives function

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,19 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    hash_table = {}
+
+    result = []
+
+    for num in a:
+        if num not in hash_table:
+            hash_table[num] = 1
+
+    for num in a:
+        if (num * -1) in hash_table and num > 0:
+            result.append(num)
+
+    print(result)
 
     return result
 
